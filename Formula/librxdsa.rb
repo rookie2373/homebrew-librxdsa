b44b1f797300f4d70bcaf022c16ea9d905500030
rushikesh <rrkundkar777@gmail.com>
2026-01-01 21:21:27 +0530

    version 2026.1.2

--- Formula/librxdsa.rb
+++ Formula/librxdsa.rb
@@ -1,7 +1,7 @@
 class Librxdsa < Formula
   desc "Lightweight C algorithms library"
   homepage "https://github.com/rookie2373/librxdsa"
-  url "https://github.com/rookie2373/librxdsa/archive/refs/tags/D2026.1.1.tar.gz"  
+  url "https://github.com/rookie2373/librxdsa/archive/refs/tags/D2026.1.2.tar.gz"  
   sha256 "b364efbae1e7c4ffda50085f3c81254a78b5b6b70885eefe43525e570768a81a"
   license "GPL-3.0-or-later"
 
