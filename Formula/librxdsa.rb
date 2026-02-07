ede36305190a29edb91a7d5356a518dbbc69dd2e
rushikesh <rrkundkar777@gmail.com>
2026-02-07 23:48:59 +0530

    version 2026.2.7.1

--- Formula/librxdsa.rb
+++ Formula/librxdsa.rb
@@ -1,14 +1,28 @@
 class Librxdsa < Formula
-  desc "Lightweight C algorithms library"
+  desc "A standalone and lightweight C algorithms library"
   homepage "https://github.com/rookie2373/librxdsa"
-  url "https://github.com/rookie2373/librxdsa/archive/refs/tags/D2026.1.2.tar.gz"
-  version "D2026.1.2"
-  sha256 "2445442f0e392150528e33a5c5eb2ca5ef3695a3ed760289d8c1a77bf53ef257"
+  head "https://github.com/rookie2373/librxdsa.git", branch: "master"
+  url "https://github.com/rookie2373/librxdsa/archive/refs/tags/R2026.2.7.1.tar.gz"
+  version "R2026.2.7.1"
+  revision 1
+  sha256 "f6db3ced9151d3e8449c13aeaf676ffc33d973d3e2f30133c1a59957a2671c2e"
   license "GPL-3.0-or-later"
 
   def install
     system "make"
     lib.install "librxdsa.a"
     include.install Dir["include/*.h"]
+  end
+
+  def caveats
+    <<~EOS
+      This library provides C algorithms. Include the header files and link with:
+        -I#{include} -L#{lib} -lrxdsa
+    EOS
+  end
+
+  livecheck do
+    url :stable
+    strategy :github_latest
   end
 end
