ff1fdaa6e8dcc46dc33c30f4f0f2d9be9b6e6c3d
rushikesh <rrkundkar777@gmail.com>
2026-02-08 00:05:57 +0530

    version 2026.2.7.1 rev 5

--- Formula/librxdsa.rb
+++ Formula/librxdsa.rb
@@ -4,8 +4,8 @@ class Librxdsa < Formula
   url "https://github.com/rookie2373/librxdsa/archive/refs/tags/R2026.2.7.1.tar.gz"
   version "R2026.2.7.1"
   sha256 "f6db3ced9151d3e8449c13aeaf676ffc33d973d3e2f30133c1a59957a2671c2e"
-  revision 4
   license "GPL-3.0-or-later"
+  revision 5
 
   def install
     system "make"
